2851adb4893e0a35dd179713cddec884fd54a48b
Mateus Peres <mateuspereslopesl@outlook.com>
2025-10-14 20:57:48 -0300

    delete.php exceptions

--- atividade/api/users/delete.php
+++ atividade/api/users/delete.php
@@ -4,13 +4,13 @@ require_once __DIR__ . '/../config.php';
 
 header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] !== 'DELETE') {
-    // EXCEPTION
+    throw new NotAllowedException();
 }
 
 $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
 if (!$id) {
-    // EXCEPTION
+    throw new BadRequestException("Missing or invalid required fields");
 }
 
 $sql = "DELETE FROM users WHERE id = :id";
@@ -19,7 +19,7 @@ $stmt->bindValue(':id', $id, PDO::PARAM_INT);
 $stmt->execute();
 
 if ($stmt->rowCount() === 0) {
-    // EXCEPTION
+    throw new NotFoundException("User Not Found");
 }
 
 http_response_code(204);
