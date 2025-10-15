a9e328d6db816f8a0982b721c87970d1e14a9cdc
Mateus Peres <mateuspereslopesl@outlook.com>
2025-10-14 21:04:53 -0300

    set_admin.php exceptions + add req test user not found

--- atividade/api/users/set_admin.php
+++ atividade/api/users/set_admin.php
@@ -4,21 +4,21 @@ require_once __DIR__ . '/../config.php';
 
 header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] !== 'PATCH') {
-    // EXCEPTION
+    throw new NotAllowedException();
 }
 
 $body = file_get_contents('php://input');
 $data = json_decode($body, true);
 
 if (json_last_error() !== JSON_ERROR_NONE) {
-    // EXCEPTION
+    throw new BadRequestException("Invalid JSON");
 }
 
 $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 $is_admin = isset($data['is_admin']) ? (int) $data['is_admin'] : null;
 
 if (!$id || !($is_admin === 0 || $is_admin === 1) ) {
-    // EXCEPTION
+    throw new BadRequestException("Missing or invalid required fields");
 }
 
 $stmt = $conn->prepare("SELECT * FROM users WHERE id = :id");
